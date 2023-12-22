d50c02e2ab42688f7fcd62c109c807d75a05308b
whodobbi <dasha.khudobina@gmail.com>
2023-12-22 13:36:25 +0600

    homework_01 done

--- homework_01/main.py
+++ homework_01/main.py
@@ -12,6 +12,10 @@ def power_numbers(*numbers):
     <<< [1, 4, 25, 49]
     """
     return [number ** 2 for number in numbers]
+    # return list(map(lambda x: x ** 2, numbers)) - можно было и так, но для такой задачи не факт, что нужно
+
+
+print(power_numbers(1, 2, 5, 7))
 
 # filter types
 ODD = "odd"
@@ -19,7 +23,13 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def is_prime(number):
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+
+def filter_numbers(nums, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +40,13 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    return [num for num in nums if (filter_type == ODD and num % 2 != 0) or
+                                    (filter_type == EVEN and num % 2 == 0) or
+                                    (filter_type == PRIME and is_prime(num))]
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(filter_numbers([1, 2, 3], "odd"))
+print(filter_numbers([2, 3, 4, 5], "even"))
+print(filter_numbers([2, 3], "prime"))
