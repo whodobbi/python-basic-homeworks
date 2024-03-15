f0960242216099de963c660f109a1bdf49808f63
whodobbi <dasha.khudobina@gmail.com>
2024-03-15 15:14:27 +0500

    homework_05 done

---
+++ homework_05/app.py
@@ -0,0 +1,30 @@
+"""
+Домашнее задание №5
+Первое веб-приложение
+
+создайте базовое приложение на Flask
+создайте index view /
+добавьте страницу /about/, добавьте туда текст
+создайте базовый шаблон (используйте https://getbootstrap.com/docs/5.0/getting-started/introduction/#starter-template)
+в базовый шаблон подключите статику Bootstrap 5 и добавьте стили, примените их
+в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
+в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.get("/", endpoint="index")
+def index():
+    return render_template("base.html")
+
+
+@app.get("/about/", endpoint="about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
